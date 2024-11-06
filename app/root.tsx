8af5e75830f995873854b0d337ab29fd89a8313b
Ryan Sobol <hello@ryansobol.com>
2024-11-06 13:06:43 -0800

    Add global pending transition UI

--- app/root.tsx
+++ app/root.tsx
@@ -2,14 +2,14 @@ import type { LinksFunction } from '@remix-run/node';
 import { json, redirect } from '@remix-run/node';
 import {
 	Form,
-	Link,
 	Links,
 	Meta,
 	NavLink,
 	Outlet,
 	Scripts,
 	ScrollRestoration,
-	useLoaderData
+	useLoaderData,
+	useNavigation
 } from '@remix-run/react';
 
 import appStylesHref from './app.css?url';
@@ -29,6 +29,7 @@ export const action = async () => {
 
 export default function App() {
 	const { contacts } = useLoaderData<typeof loader>();
+	const navigation = useNavigation();
 
 	return (
 		<html lang="en">
@@ -87,7 +88,7 @@ export default function App() {
 					</nav>
 				</div>
 
-				<div id="detail">
+				<div className={navigation.state === 'loading' ? 'loading' : ''} id="detail">
 					<Outlet />
 				</div>
 
